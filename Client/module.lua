2dd75448624de4d4b71a65b5c06a83679ee5cb0b
ou1z <59099381+ou1z@users.noreply.github.com>
2021-08-29 13:39:11 -0400

    Update module.lua

--- Client/module.lua
+++ Client/module.lua
@@ -1,14 +1,16 @@
 if not syn or not syn.websocket then
     return game.Players.LocalPlayer:Kick('Your exploit does not support websockets.')
 end
-local RELAY_ROOT = 'syn-discord-wrapper.herokuapp.com'
+local RELAY_ROOT = 'syn-discord-wrapper.herokuapp.com' -- needed, since synapse doesnt support WebsocketSecure
 
 local SynDiscord = {
     Client = {},
     Utils = {},
+    Embeds = {},
     WEBSOCKET_RELAY_SERVER = string.format('ws://%s/ws', RELAY_ROOT),
     API_ROOT = string.format('http://%s/', RELAY_ROOT)
 }
+SynDiscord.API_PROXY = SynDiscord.API_ROOT .. 'proxy/api/v9/'
 
 do -- Client Functions
     SynDiscord.Client.__index = SynDiscord.Client
@@ -69,6 +71,59 @@ do -- Client Functions
 
             if parsed.Event then
                 local Event = SynDiscord.Utils:SnakeToCamelCase(parsed.Event:lower())
+
+                if Event == 'messageCreate' then
+                    local message = parsed.Data
+
+                    message.channel = {
+                        id = message.channel_id
+                    }
+                    function message.channel.send(content, tbl)
+                        local t = {
+                            content = content
+                        }
+                        if tbl then
+                            for i,v in pairs(tbl) do
+                                t[i] = v
+                            end
+                        end
+                        local res = self:Request({
+                            Url = SynDiscord.API_PROXY .. string.format('channels/%s/messages', message.channel_id),
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'  
+                            },
+                            Body = SynDiscord.Utils:JSONEncode(t)
+                        })
+                        return res.Body
+                    end
+
+                    function message.reply(content, tbl)
+                        local t = {
+                            content = content,
+                            message_reference = {
+                                channel_id = message.channel.id,
+                                guild_id = message.guild_id,
+                                message_id = message.id
+                            }
+                        }
+                        if tbl then
+                            for i,v in pairs(tbl) do
+                                t[i] = v
+                            end
+                        end
+                        local res = self:Request({
+                            Url = SynDiscord.API_PROXY .. string.format('channels/%s/messages', message.channel_id),
+                            Method = 'POST',
+                            Headers = {
+                                ['Content-Type'] = 'application/json'  
+                            },
+                            Body = SynDiscord.Utils:JSONEncode(t)
+                        })
+                        return res.Body
+                    end
+                end
+
                 local Listeners = self.__meta__.EventListeners[Event]
                 if Listeners then
                     for i,v in pairs(Listeners) do
@@ -114,6 +169,83 @@ do -- Util Functions
             return string.upper(string.sub(s,1,1))..string.sub(s,2)
         end)
         return res 
+    end
+end
+
+do
+    SynDiscord.Embeds.__index = SynDiscord.Embeds
+
+    function SynDiscord.Embeds.Create()
+        return setmetatable({}, SynDiscord.Embeds)
+    end
+
+    function SynDiscord.Embeds:setFields(fields)
+        --[[
+            { 
+                { inline = true, name = "NAME", value = "VALUE" }, 
+                { inline = true, name = "NAME2", value = "VALUE2" }, 
+                ... 
+            }
+        ]]
+        if typeof(fields) ~= 'table' then
+            error(string.format('SetFields expects a table. Received %s.', typeof(fields)))
+            return self
+        end
+        
+        self.fields = fields
+
+        return self
+    end
+
+    function SynDiscord.Embeds:setTitle(title)
+        self.title = tostring(title)
+        return self
+    end
+
+    function SynDiscord.Embeds:setColor(hex)
+        self.color = hex
+        return self
+    end
+
+    function SynDiscord.Embeds:setThumbnail(url)
+        self.thumbnail = {
+            url = tostring(url)
+        }
+        return self
+    end
+
+    function SynDiscord.Embeds:setImage(url)
+        self.image = {
+            url = tostring(url)
+        }
+        return self
+    end
+
+    function SynDiscord.Embeds:setFooter(text, iconUrl)
+        self.footer = {
+            text = text,
+            icon_url = iconUrl
+        }
+        return self
+    end
+
+    function SynDiscord.Embeds:setUrl(url)
+        self.url = tostring(url)
+        return self
+    end
+
+    function SynDiscord.Embeds:setDescription(description)
+        self.description = tostring(description)
+        return self
+    end
+
+    function SynDiscord.Embeds:setAuthor(name, url, iconUrl)
+        self.author = {
+            name = tostring(name),
+            url = tostring(url),
+            icon_url = tostring(iconUrl)
+        }
+        return self
     end
 end
 
